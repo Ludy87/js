f8cf670f00536629a4a95dde6dcef2408947bea6
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-18 23:09:46 +0200

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,6 +1,11 @@
 $.get(isModURL, function(content) {
 	var wrapper = $(content).find(".wrapper h1").text();
 	if(wrapper === "Moderatoren-Forum") {
+		
+		if(area == "forum" && hostPathLength >= 6) {
+			console.log(area)
+		}
+		
 		$(".powerbarLinks").append('<a href="/de/android/admin/userSearch">Usersuche</a>');
 		$(".navTopLeft").append('<a href="/de/android/admin/userSearch" class="navTopLeftLink"><span>Usersuche</span></a>');
 		console.log(wrapper);
