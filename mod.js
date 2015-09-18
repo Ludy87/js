c4bf2e102f0151ce4752291d777756c317bec82b
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-18 21:43:06 +0200

    Update mod.js

--- mod.js
+++ mod.js
@@ -2,5 +2,7 @@ $.get(isModURL, function(content) {
 	var wrapper = $(content).find(".wrapper h1").text();
 	if(wrapper === "Moderatoren-Forum") {
 		console.log(wrapper);
+	} else {
+		console.log(wrapper);
 	}
 });
