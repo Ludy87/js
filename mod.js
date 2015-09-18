6d5fef3c566715e93c51b1df844d216248a83acc
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-18 21:19:48 +0200

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,17 +1,5 @@
-$.get(app + "/wortfilter.txt", function(content) {
-	$('body').removeHighlight();
-	$.each(content.split(","), function(i,v) {
-		if (v) {
-			console.log(i + " " + v);
-			if(!$("span").hasClass(".highlight")) {
-				var high = ($('body').highlight( v ));
-			}
-			$(".filter").show();
-		}
-	});
-	$(".highlight").css({"background-color": items.wordFilterColor, "color": items.wordFilterColorText});
-})
-.fail(function() {
-	$(".filter").text("Error: Wordfilterdatei ist default");
-	$(".filter").show();
-});
+if (mail == undefined && hostPathLength == 4) {
+	console.log(area)
+} else if(area == "forum" && hostPathLength >= 6) {
+	console.log(area)
+}
