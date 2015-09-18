b64f5fe617b7ceff02883b957e62d7dc4bf0da41
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-18 19:40:36 +0200

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,1 +1,3 @@
+$(".powerbarLinks").append('<a href="/de/android/admin/userSearch">Usersuche</a>');
+$(".navTopLeft").append('<a href="/de/android/admin/userSearch" class="navTopLeftLink"><span>Usersuche</span></a>');
 console.log(mail)
