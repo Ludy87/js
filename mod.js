36bcc3f164a9566c057d5b97433efa104ba04a04
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-18 20:00:30 +0200

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,8 +1,2 @@
 $(".powerbarLinks").append('<a href="/de/android/admin/userSearch">Usersuche</a>');
 $(".navTopLeft").append('<a href="/de/android/admin/userSearch" class="navTopLeftLink"><span>Usersuche</span></a>');
-
-	$( "body" ).delegate( "button.addReplyComment, button.cancelReply", "click", function() {
-		var text = $(this).data("text");
-		$("div.siteload").remove();
-		return true;
-	});
