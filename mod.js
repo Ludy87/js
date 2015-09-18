dce5747b03d4fe5c56a132980d044c1b8b5d7eee
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-18 21:37:52 +0200

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,5 +1,6 @@
 if (mail == undefined && hostPathLength == 4) {
-	console.log(area)
+	console.log(area);
+	console.log(Math.floor((Math.random() * 10) + 1));
 } else if(area == "forum" && hostPathLength >= 6) {
 	console.log(area)
 }
