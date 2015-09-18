7972e932d254983a1facfe3bb9d6cac62565f340
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-18 20:48:38 +0200

    Update mod.js

--- mod.js
+++ mod.js
@@ -1,2 +1,18 @@
 $(".powerbarLinks").append('<a href="/de/android/admin/userSearch">Usersuche</a>');
 $(".navTopLeft").append('<a href="/de/android/admin/userSearch" class="navTopLeftLink"><span>Usersuche</span></a>');
+$.get(isModURL, function(data) {
+	var wrapper = $(data).find(".wrapper h1").text();
+	var host = window.location.href;
+	var hostSplit = host.split("/");
+	var hostPathLength = hostSplit.length;
+	var apit = hostSplit[3];
+	var mail = hostSplit[6];
+	if(wrapper === "Moderatoren-Forum") {
+		if (mail == undefined && hostPathLength == 4) {
+			
+		} else if(apit == "forum" && hostPathLength >= 6) {
+			console.log(apit)
+		}
+		
+	}
+});
