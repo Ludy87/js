509ffc2e1245ecc59de7635d368c0c56762d4c1d
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-19 10:10:35 +0200

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,6 +1,6 @@
 var textZeile = $("#formRegister").find(".group")[2];
 //$(textZeile).before("<div class=\"group bottommargin10\"><a href=\"#\" style=\"margin: 1px; padding: 2px !important;\" class=\"defaultButton btn-primary-small padding-y-small\">StandardTexte</a><div class=\"siteload\" style=\"display: none;\"><ul class=\"myMenu\"></ul><div style=\"clear:both;\"></div></div>");
-$(textZeile).before("<div class=\"group bottommargin10\"><div class=\"col3\"><label><strong>StandardTexte</strong></label></div><div class=\"col9 last\"><div class=\"siteload\" style=\"display: none;\"><ul class=\"myMenu\"></ul><div style=\"clear:both;\"></div></div></div>");
+$(textZeile).before("<div class=\"group bottommargin10\"><div class=\"col3\"><label><strong>StandardTexte</strong></label></div><div class=\"col9 last\"><div class=\"siteload\" style=\"display: inline-block;\"><ul class=\"myMenu\"></ul><div style=\"clear:both;\"></div></div></div>");
 
 myThis.chrome.storage.local.get(null, function(items) {
 	$.each(items, function(index, value) {
