a51bee15c37bd9f37a250d5d6e1d3d0016c3a29f
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-19 06:14:05 +0200

    Update mod_modi.js

--- mod_modi.js
+++ mod_modi.js
@@ -1,5 +1,20 @@
 
 if(area == "forum" && hostPathLength >= 6) {
+	var formData = {searchIpAddress:"127.0.0.1",type:"ipAddress"}; 
+	
+	$.ajax({
+	    url : userSearch,
+	    type: "POST",
+	    data : formData,
+	    success: function(data, textStatus, jqXHR)
+	    {
+	        console.log(data)
+	    },
+	    error: function (jqXHR, textStatus, errorThrown)
+	    {
+	 
+	    }
+	});
 	$(".containerContent").before('<iframe src="/de/android/admin/userSearch" width="220" height="350" style="border: 0;"></iframe>');
 }
 $(".powerbarLinks").append('<a href="/de/android/admin/userSearch">Usersuche</a>');
