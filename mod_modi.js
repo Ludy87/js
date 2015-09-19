43d63409938ffa742ac4ab7c75e93eb1effbad05
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-19 06:05:48 +0200

    Update mod_modi.js

--- mod_modi.js
+++ mod_modi.js
@@ -1,6 +1,6 @@
 
 if(area == "forum" && hostPathLength >= 6) {
-	
+	$(".containerContent").before("<p>Tesz</p>");
 }
 $(".powerbarLinks").append('<a href="/de/android/admin/userSearch">Usersuche</a>');
 $(".navTopLeft").append('<a href="/de/android/admin/userSearch" class="navTopLeftLink"><span>Usersuche</span></a>');
