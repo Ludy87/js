2d15ad23fdebe35513877195f7894f9494877912
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-19 06:27:37 +0200

    Update mod_modi.js

--- mod_modi.js
+++ mod_modi.js
@@ -8,7 +8,8 @@ if(area == "forum" && hostPathLength >= 6) {
 	    data : formData,
 	    success: function(data, textStatus, jqXHR)
 	    {
-	        console.log($(data).find(".adminBox").html())
+	        console.log($(data).find(".adminBox").html());
+	        $(".wrapper, .forumWrapper").before('<iframe>'+$(data).find(".adminBox").html()+'</iframe>')
 	    },
 	    error: function (jqXHR, textStatus, errorThrown)
 	    {
