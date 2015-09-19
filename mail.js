16343ef6650b3222be0a55a92c112bb5ff0a2980
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-19 10:05:21 +0200

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,5 +1,6 @@
 var textZeile = $("#formRegister").find(".group")[2];
-$(textZeile).before("<a href=\"#\" style=\"margin: 1px; padding: 2px !important;\" class=\"defaultButton btn-primary-small padding-y-small\">StandardTexte</a><div class=\"siteload\" style=\"display: none;\"><ul class=\"myMenu\"></ul><div style=\"clear:both;\"></div></div>");
+//$(textZeile).before("<div class=\"group bottommargin10\"><a href=\"#\" style=\"margin: 1px; padding: 2px !important;\" class=\"defaultButton btn-primary-small padding-y-small\">StandardTexte</a><div class=\"siteload\" style=\"display: none;\"><ul class=\"myMenu\"></ul><div style=\"clear:both;\"></div></div>");
+$(textZeile).before("<div class=\"group bottommargin10\"><div class=\"col3\"><a href=\"#\" style=\"margin: 1px; padding: 2px !important;\" class=\"defaultButton btn-primary-small padding-y-small\">StandardTexte</a></div><div class=\"col9 last\"><div class=\"siteload\" style=\"display: none;\"><ul class=\"myMenu\"></ul><div style=\"clear:both;\"></div></div></div>");
 
 myThis.chrome.storage.local.get(null, function(items) {
 	$.each(items, function(index, value) {
