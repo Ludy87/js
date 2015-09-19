54fe207f1fce6794314c1ff692cf1807be155fcd
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-19 07:02:45 +0200

    Update mod_modi.js

--- mod_modi.js
+++ mod_modi.js
@@ -6,12 +6,11 @@ if(area == "forum" && hostPathLength >= 6) {
 	    success: function(data, textStatus, jqXHR)
 	    {
 	    	var form = $(data).find(".adminBox form")[2];
-	    	console.log(form);
 	    	$(form).find('input[type=submit]').attr('class', 'btn-primary-small padding-y-small');
 	    	$(form).find('label').attr('style',"");
 	    	$(form).find('input#searchIpAddress').attr('size',"");
-	        console.log($(form).html());
-	        $(".forumThreadAdLeaderboardTop").after('<div>'+$(form).html()+'</div>')
+	        console.log($(form);
+	        $(".forumThreadAdLeaderboardTop").after('<div>'+form+'</div>')
 	    },
 	    error: function (jqXHR, textStatus, errorThrown)
 	    {
