b30d7969ecd2e35d6a3840d2182714c552967a4c
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-19 09:24:47 +0200

    Update mod_modi.js

--- mod_modi.js
+++ mod_modi.js
@@ -20,6 +20,7 @@ if(area == "forum" && hostPathLength >= 6) {
 	    }
 	});
 }
+console.log(area)
 $(".powerbarLinks").append('<a href="/de/android/admin/userSearch">Usersuche</a>');
 $(".navTopLeft").append('<a href="/de/android/admin/userSearch" class="navTopLeftLink"><span>Usersuche</span></a>');
 console.log(wrapper);
