3865e0a6c1e6010c28e2bc0216c98947ac153a2d
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-19 09:58:01 +0200

    Update mail.js

--- mail.js
+++ mail.js
@@ -1,16 +1,7 @@
 var textZeile = $("#formRegister").find(".group")[2];
 $(textZeile).before("<a href=\"#\" style=\"margin: 1px; padding: 2px !important;\" class=\"defaultButton btn-primary-small padding-y-small\">StandardTexte</a><div class=\"siteload\" style=\"display: none;\"><ul class=\"myMenu\"></ul><div style=\"clear:both;\"></div></div>");
-$( "body" ).delegate( "a.defaultButton", "click", function() {
-	console.log($(".siteload"))//.toggle();
-	return false;
-});
-myThis.chrome.storage.local.get(null, function(items) {
-	var allKeys = Object.keys(items);
 
-	var zahl = $("a.schreib").length;
-	$.each($("a.schreib"), function(i,v) {
-		//console.log(i + " : " + zahl)
-	})
+myThis.chrome.storage.local.get(null, function(items) {
 	$.each(items, function(index, value) {
 		if(value != "" && index != "" ) {
 			if($(".schreib").text() != index) {
@@ -21,9 +12,9 @@ myThis.chrome.storage.local.get(null, function(items) {
 	});
 });
 $( "body" ).delegate(".schreib", "click", function() {
-	var text = $(this).data("text");
-	text = decodeURIComponent(text);
-	if(text.indexOf("%Name%") >= 0) {
+	var text = $("textarea#text").val(); //$(this).data("text");
+	text += decodeURIComponent($(this).data("text"));
+	if(text.indexOf("%Name%") >= 0 || text.indexOf("%te%") >= 0) {
 		text = text.replace('%Name%' ,$("#receiverName").val());
 		console.log(text)
 	}
