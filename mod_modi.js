67930783c9e10b7e33f0aab71781c5773cb1e192
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-19 06:07:43 +0200

    Update mod_modi.js

--- mod_modi.js
+++ mod_modi.js
@@ -1,6 +1,6 @@
 
 if(area == "forum" && hostPathLength >= 6) {
-	$(".containerContent").before("<p>Tesz</p>");
+	$(".containerContent").before('<iframe src="/de/android/admin/userSearch" width="220" height="350" style="border: 0;"></iframe>');
 }
 $(".powerbarLinks").append('<a href="/de/android/admin/userSearch">Usersuche</a>');
 $(".navTopLeft").append('<a href="/de/android/admin/userSearch" class="navTopLeftLink"><span>Usersuche</span></a>');
