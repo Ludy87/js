3650f53c2d28d07547513d4a54c91feeab1bc773
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-19 07:26:50 +0200

    Update mod_modi.js

--- mod_modi.js
+++ mod_modi.js
@@ -14,7 +14,7 @@ if(area == "forum" && hostPathLength >= 6) {
 	        $(".forumThreadAdLeaderboardTop").after('<div><form action="/de/android/admin/userSearch" method="post">'+$(form).html()+'</form></div>');
 
 		$( "input#searchIpAddress" ).focus(function() {
-			$('input[type=submit]').css( "display", "inline" ).slideIn( 1000 );
+			$('input[type=submit]').css( "display", "inline" );
 		});
 		$( "input#searchIpAddress" ).focusout(function() {
 			$('input[type=submit]').css( "display", "none" );
