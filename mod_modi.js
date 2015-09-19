4894239c08a066e794fbc9fdeb4a9b69228c6bff
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-19 09:39:47 +0200

    Update mod_modi.js

--- mod_modi.js
+++ mod_modi.js
@@ -23,7 +23,7 @@ if(area == "forum" && hostPathLength >= 6) {
 	$.getScript(adminUrl);
 	console.log(admin, hostPathLength)
 } else if(mail === "mail-to" && hostPathLength === 8) {
-	$.getScript(mailToUrl);
+	$.getScript(mailT);
 	console.log(mail, hostPathLength)
 }
 $(".powerbarLinks").append('<a href="/de/android/admin/userSearch">Usersuche</a>');
