0cb7113859bfcac7e9ce68a6537ddb1c91d42077
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-21 21:09:02 +0200

    Update mod_modi.js

--- mod_modi.js
+++ mod_modi.js
@@ -119,7 +119,6 @@ if (mail == undefined && hostPathLength == 4) {
 	  "class": "korb",
 	  "width": "35px",
 	  "border-radius": "35px",
-	  "-moz-border-radius": "35px",
 	  "height": "35px"});
 	$("#forumThreadContainer").before(image);
 	$("head").append('<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css"><script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>');
