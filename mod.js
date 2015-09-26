884b66ff67c18cb74b929012e2cc75b578b15ba6
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-26 22:10:55 +0200

    Update mod.js

--- mod.js
+++ mod.js
@@ -16,6 +16,7 @@ function Android () {
     };
     
     this.top = function() {
+    	$('.forumThreadListPageLabelFixed, .instantScrollAnchor, .forumThreadListPageLabelFixedInside').remove();
 	$("body").append('<a href="#0" class="cd-top" style="border-radius: 50px; z-index: 100; left: 50%;">Top</a>');
 	var offset = 300,
 	offset_opacity = 1200,
