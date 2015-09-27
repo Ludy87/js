df80c8d0ec4eda9b6b2c002949a922ba4227a39e
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-27 19:56:31 +0200

    Update forum.js

--- forum.js
+++ forum.js
@@ -65,7 +65,12 @@
 	$.fn.forum.writeSite = function(_this) {
 		setTimeout(function() {
 			var editor = $(_this).find("div.forumEditor");
-			console.log($(editor).prepend("<a href=\"#\" style=\"margin: 1px; padding: 2px !important;\" class=\"defaultButton btn-primary-small padding-y-small\">StandardTexte</a><div class=\"siteload\" style=\"display: none;\"><ul class=\"myMenu\"></ul><div style=\"clear:both;\"></div></div>"))
+			($(editor).prepend("<a href=\"#\" style=\"margin: 1px; padding: 2px !important;\" class=\"defaultButton btn-primary-small padding-y-small\">StandardTexte</a><div class=\"siteload\" style=\"display: none;\"><ul class=\"myMenu\"></ul><div style=\"clear:both;\"></div></div>"))
+			chrome.storage.local.get(null, function(items) {
+				var allKeys = Object.keys(items);
+				console.log(allKeys)
+			});
+			
 		}, 1500);
 	/**$("div.forumEditor").prepend("<a href=\"#\" style=\"margin: 1px; padding: 2px !important;\" class=\"defaultButton btn-primary-small padding-y-small\">StandardTexte</a><div class=\"siteload\" style=\"display: none;\"><ul class=\"myMenu\"></ul><div style=\"clear:both;\"></div></div>");
 		chrome.storage.local.get(null, function(items) {
