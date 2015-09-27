73dc54d5f57c300a3e2fbff30e74cbc4bb8dbb1b
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-27 13:33:58 +0200

    Update all.js

--- all.js
+++ all.js
@@ -1,6 +1,6 @@
 (function($) {
   $.fn.all = function() {
     $('.forumThreadListPageLabelFixed, .instantScrollAnchor, .forumThreadListPageLabelFixedInside').remove();
-  	$("body").append('<a href="#0">Top</a>');
+  	$("body").append('<a href="#0" class="cd-top" style="border-radius: 50px; z-index: 100; left: 50%;">Top</a>');
   }
 }(jQuery));
