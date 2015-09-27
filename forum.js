72862159f720e8f9811e1271f57698730db7e183
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-27 14:13:48 +0200

    Update forum.js

--- forum.js
+++ forum.js
@@ -3,7 +3,26 @@
 	$.fn.forum = function() {
 		var item;
 		this.each(function() {
-		var _this = this;
+			var header = ($(_this).find(".threadPostHeader"));
+			var userID = $(header).find(".threadPostAuthorNameLink").attr("href").split("/")[2];
+			var postid = ($(_this).data('postid'));
+			//$(this).find(".threadPostHeader").next().remove();
+			if(userID == "android") {
+				userID = $(header).find(".threadPostAuthorNameLink").attr("href").split("=")[1];
+			}
+			if(userID != "android") {
+				if(userID == "4361215" || userID == "2927890") {
+					$(header).find(".threadPostAuthorName .user-badges span").first().append(" DEV");
+				}
+				$.fn.forum.warning(header, userID)
+				$.fn.forum.ip(header, postid, userID); 
+				$(header).after('<a class="btn-primary-small padding-y-small" style="margin-left: 1px;" href="' + adminSite + userID + '">Adminseite</a>');
+				$(header).after('<a class="btn-primary-small padding-y-small" style="margin-left: 1px;" href="' + pnChange + userID + '">PN-Changer</a>');
+				$(header).after('<a class="btn-primary-small padding-y-small" style="margin-left: 1px;" href="' + mailTo + userID + '">Mail schreiben</a>');
+		
+			}
+		});
+		
 		chrome.storage.sync.get({
 			teVisable: false,
 			adminSiteVisable: false,
@@ -14,27 +33,7 @@
 			viewWarningVisable: false
 		}, function(items) {
 			item = (items)
-			console.log(item)
-				var header = ($(_this).find(".threadPostHeader"));
-				var userID = $(header).find(".threadPostAuthorNameLink").attr("href").split("/")[2];
-				var postid = ($(_this).data('postid'));
-				//$(this).find(".threadPostHeader").next().remove();
-				if(userID == "android") {
-					userID = $(header).find(".threadPostAuthorNameLink").attr("href").split("=")[1];
-				}
-				if(userID != "android") {
-					if(userID == "4361215" || userID == "2927890") {
-						$(header).find(".threadPostAuthorName .user-badges span").first().append(" DEV");
-					}
-					$.fn.forum.warning(header, userID)
-					$.fn.forum.ip(header, postid, userID); 
-					$(header).after('<a class="btn-primary-small padding-y-small" style="margin-left: 1px;" href="' + adminSite + userID + '">Adminseite</a>');
-					$(header).after('<a class="btn-primary-small padding-y-small" style="margin-left: 1px;" href="' + pnChange + userID + '">PN-Changer</a>');
-					$(header).after('<a class="btn-primary-small padding-y-small" style="margin-left: 1px;" href="' + mailTo + userID + '">Mail schreiben</a>');
-			
-				}
-		});
-			
+			console.log(item)	
 		});
 	}
 	
