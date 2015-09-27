8dc896a93f97d317b5f68e6aeba29f0996ebb99d
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-27 18:14:54 +0200

    Update forum.js

--- forum.js
+++ forum.js
@@ -13,8 +13,7 @@
 				viewWarningVisable: false
 			}, function (obj) {
 				var header = ($(_this).find(".threadPostHeader"));
-				console.log($(header + ".isThreadAuthor .threadPostAuthorName .threadPostAuthorNameLink"))
-				console.log($(_this + ".isThreadAuthor .threadPostAuthorName .threadPostAuthorNameLink"))
+				console.log($(_this))
 				var userID = $(header).find(".threadPostAuthorNameLink").attr("href").split("/")[2];
 				var postid = ($(_this).data('postid'));
 				//$(this).find(".threadPostHeader").next().remove();
