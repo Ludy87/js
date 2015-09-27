53307b45d3df0eb041c80545d63088adec696d0a
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-27 14:45:04 +0200

    Update forum.js

--- forum.js
+++ forum.js
@@ -1,19 +1,29 @@
 (function($) {
 	
 	$.fn.forum = function() {
-		var bugVal;
-		chrome.storage.sync.get({
-			teVisable: false,
-			adminSiteVisable: false,
-			mailToVisable: false,
-			pnChangerVisable: false,
-			warningVisable: false,
-			ipVisable: false,
-			viewWarningVisable: false
-		}, function (obj) {
-			bugVal = obj;
-		});
-		console.log(bugVal)
+		function workWithBugVal(val) {
+		    console.log(val)
+		}
+		
+		function getBugVal(callback) {
+		    	var bugVal = "";
+		
+			chrome.storage.sync.get({
+				teVisable: false,
+				adminSiteVisable: false,
+				mailToVisable: false,
+				pnChangerVisable: false,
+				warningVisable: false,
+				ipVisable: false,
+				viewWarningVisable: false
+			}, function (obj) {
+			        console.log(obj.bugId);
+			        bugVal = obj.bugId;
+			        callback(bugVal);
+		    	});
+		}
+		
+		getBugVal(workWithBugVal);
 		this.each(function() {
 			var header = ($(this).find(".threadPostHeader"));
 			var userID = $(header).find(".threadPostAuthorNameLink").attr("href").split("/")[2];
