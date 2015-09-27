e6f3f6d440aeea074e085a2cd3f20404a5cf8f85
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-27 13:47:14 +0200

    Update all.js

--- all.js
+++ all.js
@@ -1,7 +1,12 @@
 (function($) {
 	$.fn.all = function() {
 		$.fn.all.top();
+		$.fn.all.powerBar();
 	}
+	$.fn.all.powerBar = function() {
+	    	$('.headerMain, .headerMain-forum').css('margin-top', '30px');
+	    	$('.powerbarContainer').css('position', 'fixed').css('margin-top', '-30px').css('z-index', '100').css('width', '100%');
+	};
 	$.fn.all.top = function() {
 		$('.forumThreadListPageLabelFixed, .instantScrollAnchor, .forumThreadListPageLabelFixedInside').remove();
 		$("body").append('<a href="#0" class="cd-top" style="border-radius: 50px; z-index: 100; left: 50%;">Top</a>');
