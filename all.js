dfeb84013a1c5d78ae908cb49624c36df62992f1
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-27 13:38:59 +0200

    Update all.js

--- all.js
+++ all.js
@@ -6,8 +6,37 @@
   	offset_opacity = 1200,
   	scroll_top_duration = 700,
   	$back_to_top = $('.cd-top');
-  	$(window).scroll(function(){
-  	  console.log(this)
-  	})
+  	top();
   }
 }(jQuery));
+
+function top() {
+    	$('.forumThreadListPageLabelFixed, .instantScrollAnchor, .forumThreadListPageLabelFixedInside').remove();
+	$("body").append('<a href="#0" class="cd-top" style="border-radius: 50px; z-index: 100; left: 50%;">Top</a>');
+	var offset = 300,
+	offset_opacity = 1200,
+	scroll_top_duration = 700,
+	$back_to_top = $('.cd-top');
+
+	$(window).scroll(function(){
+		( $(this).scrollTop() > offset ) ? $back_to_top.addClass('cd-is-visible') : $back_to_top.removeClass('cd-is-visible cd-fade-out');
+		if( $(this).scrollTop() > offset_opacity ) { 
+			$back_to_top.addClass('cd-fade-out');
+		}
+	});
+
+	$back_to_top.on('click', function(event){
+		event.preventDefault();
+		$('body,html').animate({
+			scrollTop: 0 ,
+		 	}, scroll_top_duration
+		);
+	});
+    	$.ajax({
+	    url: 'https://raw.githubusercontent.com/Ludy87/js/master/backToTop.css',
+	    dataType: 'text',
+	    success: function(data) {
+	        $('<style type="text/css">\n' + data + '</style>').appendTo("head");                    
+	    }                  
+	});
+};
