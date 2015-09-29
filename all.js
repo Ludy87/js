7b02a4a3fbe2a73d6135ee169090e20b8c151e4b
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-29 09:14:43 +0200

    Update all.js

--- all.js
+++ all.js
@@ -12,7 +12,7 @@
 		$("body").append('<a href="#0" class="cd-top" style="border-radius: 50px; z-index: 100; left: 50%;">Top</a>');
 		var offset = 300,
 		offset_opacity = 1200,
-		scroll_top_duration = 300, // 700
+		scroll_top_duration = 700, // 700
 		$back_to_top = $('.cd-top');
 	
 		$(window).scroll(function(){
