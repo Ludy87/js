768b59cda322acd8c184bf1947f51f4b3df3a59e
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-29 09:14:18 +0200

    Update all.js

--- all.js
+++ all.js
@@ -10,9 +10,9 @@
 	$.fn.all.top = function() {
 		$('.forumThreadListPageLabelFixed, .instantScrollAnchor, .forumThreadListPageLabelFixedInside').remove();
 		$("body").append('<a href="#0" class="cd-top" style="border-radius: 50px; z-index: 100; left: 50%;">Top</a>');
-		var offset = 000,
+		var offset = 300,
 		offset_opacity = 1200,
-		scroll_top_duration = 700,
+		scroll_top_duration = 300, // 700
 		$back_to_top = $('.cd-top');
 	
 		$(window).scroll(function(){
