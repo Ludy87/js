d4883b532de06437863af34518553c818e2cbb65
Ludy87 <Ludy87@users.noreply.github.com>
2015-09-29 11:58:43 +0200

    Update mod.js

--- mod.js
+++ mod.js
@@ -65,7 +65,7 @@ function Android () {
     this.top = function() {
     	$('.forumThreadListPageLabelFixed, .instantScrollAnchor, .forumThreadListPageLabelFixedInside').remove();
 	$("body").append('<a href="#0" class="cd-top" style="border-radius: 50px; z-index: 100; left: 50%;">Top</a>');
-	var offset = 100, //300
+	var offset = 10, //300
 	offset_opacity = 1200,
 	scroll_top_duration = 700,
 	$back_to_top = $('.cd-top');
