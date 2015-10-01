e5fa0682f53951eef28fb1c0e6fed03e9b216834
Ludy87 <Ludy87@users.noreply.github.com>
2015-10-01 10:49:40 +0200

    Update mod.js

--- mod.js
+++ mod.js
@@ -3,13 +3,13 @@ var mailT = mailToUrl;
 function Android () {
 	
 	this.searchBar = function() {
-		var script = document.createElement('script');
+	/**	var script = document.createElement('script');
 		script.text = '(function() {var cx = "006006678927633944778:-li7chryqxa";var gcse = document.createElement("script");gcse.type = "text/javascript";gcse.async = true;gcse.src = (document.location.protocol == "https:" ? "https:" : "http:") + "//cse.google.com/cse.js?cx=" + cx;var s = document.getElementsByTagName("script")[0];s.parentNode.insertBefore(gcse, s);})();';
 		document.getElementsByTagName('head')[0].appendChild(script);
 		var form = '<form id="cse-search-box" action="https://google.com/cse"><input type="hidden" name="cx" value="006006678927633944778:-li7chryqxa" /><input type="hidden" name="ie" value="UTF-8" /><input type="text" name="q" size="31" /><input type="submit" name="sa" value="such" /></form>';
 	//	$("body").prepend('<gcse:searchresults></gcse:searchresults>');
 	//	$('.searchip ul').append('<li class="forumSideboxItem">T</li>');
-		console.log($('section.searchip ul').text())
+		console.log($('section.searchip ul').text())**/
 		//$(".forumSidebar").prepend('<section class="forumSidebox searchgoogle"><h2>Suche</h2><ul><li class="forumSideboxItem"><gcse:searchbox></gcse:searchbox></li></ul></section>');
 		//$(".forumSidebar").prepend('<section class="forumSidebox searchgoogle"><h2>Suche</h2><ul><li class="forumSideboxItem"><gcse:searchbox></gcse:searchbox><gcse:searchresults></gcse:searchresults></li></ul></section>');
 	}
