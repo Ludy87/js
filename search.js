91743814825ffb8602fcf3ab6534fb6eac04976a
Ludy87 <Ludy87@users.noreply.github.com>
2015-10-01 09:18:57 +0200

    Update search.js

--- search.js
+++ search.js
@@ -1,13 +1,11 @@
-<script>
-  (function() {
-    var cx = '006006678927633944778:-li7chryqxa';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
-        '//cse.google.com/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-  })();
-</script>
+(function() {
+  var cx = '006006678927633944778:-li7chryqxa';
+  var gcse = document.createElement('script');
+  gcse.type = 'text/javascript';
+  gcse.async = true;
+  gcse.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') +
+      '//cse.google.com/cse.js?cx=' + cx;
+  var s = document.getElementsByTagName('script')[0];
+  s.parentNode.insertBefore(gcse, s);
+})();
 <gcse:search></gcse:search>
