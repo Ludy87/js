540b8e5aaf56f7f540b66ad8de300e21cf753412
Ludy87 <Ludy87@users.noreply.github.com>
2015-12-02 14:29:32 +0100

    Update mod_modi.js

--- mod_modi.js
+++ mod_modi.js
@@ -42,8 +42,8 @@ if (mail == undefined && hostPathLength == 4 && area !== "leaderboard") {
 						.done(function() {
 							if($i==0) {
 								$( "span.highlight" ).each(function( index ) {
-									console.log("marker" + index);
-									$( this ).attr("id", "marker" + index);
+									$idx = ("marker" + index);
+									$( this ).attr("id", $idx);
 								});
 							}
 							$i++;
