599fccc0cc9e4cd244df11d21d421f6de38d1424
Ludy87 <Ludy87@users.noreply.github.com>
2015-12-02 12:47:08 +0100

    Update mod_modi.js

--- mod_modi.js
+++ mod_modi.js
@@ -17,13 +17,14 @@ if (mail == undefined && hostPathLength == 4 && area !== "leaderboard") {
 				if(items.wordFilter) {
 					$(document).ready(function() {
 						$(".filter").remove()
-						$("body").prepend("<div class=\"filter\" style=\"position: fixed; z-index: 10000; background-color: red; padding: 10px; bottom: 50%; color: white; font-size: 1.2em; display: none;\">Wordfiltertreffer</div>")
 						//var app = "chrome-extension://ihngcbdenildjnpeheelhodmnnfgfmnl";
 						$.get(chrome.extension.getURL("/wortfilter.txt"), function(content) {
 							$('body').removeHighlight();
 							$.each(content.split(","), function(i,v) {
 								if (v) {
 									if(!$("span").hasClass(".highlight")) {
+										$("body").prepend("<div class=\"filter\" style=\"position: fixed; z-index: 10000; background-color: red; padding: 10px; bottom: 50%; color: white; font-size: 1.2em; display: none;\">Wordfiltertreffer</div>")
+					
 										console.log(i + " " + v);
 										var high = ($('body').highlight( v ));
 										$(".filter").show();
