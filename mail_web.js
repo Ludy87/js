ff3f5f46f7b19ae47c6b155d387f6f2cc67210f8
Ludy87 <Ludy87@users.noreply.github.com>
2015-12-14 21:10:12 +0100

    Update mail_web.js

--- mail_web.js
+++ mail_web.js
@@ -3,7 +3,7 @@ var textZeile = $("#formRegister").find(".group")[2];
 $(textZeile).before("<div class=\"group bottommargin10\"><div class=\"col3\"><label><strong>StandardTexte</strong></label></div><div class=\"col9 last\"><div class=\"siteload\" style=\"display: inline-block;\"><ul class=\"myMenu\"></ul><div style=\"clear:both;\"></div></div></div>");
 
 myThis.chrome.storage.local.get("text", function(items) {
-	$.each(items["text", function(index, value) {
+	$.each(items["text"], function(index, value) {
 		if(value != "" && index != "" ) {
 			if($(".schreib").text() != index) {
 				var myvar = '<li><a href="#" class="schreib btn-primary-small padding-y-small" style="margin: 1px; padding: 2px !important;" data-text="' + value + '">' + decodeURIComponent(index) + '</a></li>';
