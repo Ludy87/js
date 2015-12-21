35364cd45d49ae6a9ed4a35fb9f09fe9328af107
Ludy87 <Ludy87@users.noreply.github.com>
2015-12-21 11:44:57 +0100

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,10 @@
-
-	$.get( url_l, function( data ) {
-		console.log(data)
-	});
+$.ajax({
+	url: url_l,
+	type: "GET",
+	dataType: "json",
+	contentType: 'text/json-comment-filtered;charset=UTF-8',
+	cache: false,
+	success: function (data, status, error) {
+		eval(data);
+	}
+});
