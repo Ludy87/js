06d13ed7aa1bfea9f284ab8e878551f9d00f3fc3
Ludy87 <Ludy87@users.noreply.github.com>
2015-12-22 15:23:11 +0100

    Update mod_modi_web.js

--- mod_modi_web.js
+++ mod_modi_web.js
@@ -1,6 +1,9 @@
 if (mail == undefined && hostPathLength == 4 && area !== "leaderboard") {
 	console.log("!mail")
 	var link = $(".commentContainer header a").each(function(index, value) {
+		if(indey == 50) {
+			return false;
+		}
 		if(!$(value).hasClass('commentLink')) {
 			var linkId = ($(value).attr("href").split("/")[2])
 			var $header = $(this).parent();
